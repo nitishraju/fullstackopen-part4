3f7b9e388682f44b2d25e54d3f070fe3ac3343da
Nitish Raju <nitishraju@outlook.com>
2021-01-02 16:48:31 -0500

    Removed error logging when testing

--- utils/middleware.js
+++ utils/middleware.js
@@ -21,7 +21,10 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
+  if (process.env.NODE_ENV !== 'test') {
+    logger.error(error.message)
+  }
+
   if (error.name === 'ValidationError') {
     return response.status(400).json({
       error: error.message
